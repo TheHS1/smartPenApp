8d33b765b1db258a63c891d87c36d3fb549ec861
Harjot Singh <harjot.singh0712@gmail.com>
2024-12-23 20:07:56 -0800

    BLE setup with device selection

--- app/screens/Main.tsx
+++ app/screens/Main.tsx
@@ -1,9 +1,38 @@
 import { useState, useRef } from "react";
-import { GestureResponderEvent, View } from "react-native";
+import { GestureResponderEvent, TouchableOpacity, View, Text} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Svg, Path } from 'react-native-svg'
+import useBLE from '../useBLE'
+import DeviceModal from "./DeviceConnectionModal";
 
 export default function Main() {
+
+    const {
+        requestPermissions,
+        scanForPeripherals,
+        allDevices,
+        connectToDevice,
+        connectedDevice,
+        data,
+        disconnectFromDevice
+    } = useBLE();
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+    const scanForDevices = async () => {
+        const isPermissionsEnabled = await requestPermissions();
+        if (isPermissionsEnabled) {
+            scanForPeripherals();
+        }
+    }
+
+    const hideModal = () => {
+        setIsModalVisible(false);
+    }
+
+    const openModal = async () => {
+        scanForDevices();
+        setIsModalVisible(true);
+    }
 
     const [paths, setPaths] = useState<string[][]>([])
     const [curDrawn, setCurDrawn] = useState<string[]>([]);
@@ -11,12 +40,12 @@ export default function Main() {
     const paintRef = useRef(null);
 
     const updatePath = (event: GestureResponderEvent | PointerEvent) => {
-        const rect = paintRef.current.getBoundingClientRect();
         // get location of finger press or mouse move
         if((event as GestureResponderEvent).nativeEvent.locationX) {
             var x = (event as GestureResponderEvent).nativeEvent.locationX;
             var draw = true
         } else {
+            var rect = paintRef.current.getBoundingClientRect();
             var x = (event as PointerEvent).clientX - rect.left;
             var draw = drawing
         }
@@ -29,7 +58,6 @@ export default function Main() {
 
         if (draw) {
             const curPath = [...curDrawn];
-            console.log(y)
 
             // Due to SVG format, make sure that points are in MX,Y format
             const point = `${curPath.length === 0 ? 'M' : ""}${x.toFixed(0)},${y.toFixed(0)}`;
@@ -55,7 +83,8 @@ export default function Main() {
 
     return (
       <SafeAreaView className="flex h-full w-full">
-        <View ref={paintRef} onTouchMove={updatePath} onTouchEnd={savePath} onPointerMove={updatePath} onPointerDown={setDraw} onPointerUp={unsetDraw} className="border-blue-500 border-4 w-full h-full">
+        {connectedDevice ? (
+        <View ref={paintRef} onTouchMove={updatePath} onTouchEnd={savePath} onPointerMove={updatePath} onPointerDown={setDraw} onPointerUp={unsetDraw} className="border-blue-500 border-4 w-full h-1/2">
             <Svg className="h-full">
             <Path
                 d={curDrawn.join('\n')}
@@ -74,8 +103,27 @@ export default function Main() {
                 strokeLinecap="round"
             />
             </Svg>
+            <Text>{data}</Text>
         
         </View>
+        ) : (
+        <View>
+            <Text className="text-center text-lg">Please connect your smart pen device</Text>
+            <TouchableOpacity
+                onPress={openModal}
+            >
+                <Text className="text-center bg-blue-500 p-5 m-10 text-white">
+                    Connect
+                </Text>
+                <DeviceModal
+                    closeModal={hideModal}
+                    visible={isModalVisible}
+                    connectToPeripheral={connectToDevice}
+                    devices={allDevices}
+                />
+            </TouchableOpacity>
+        </View>
+        )}
       </SafeAreaView>
     )
 }
---
+++ app/useBLE.ts
@@ -0,0 +1,172 @@
+import { useMemo, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+import { BleError, BleManager, Characteristic, Device } from "react-native-ble-plx"
+
+import * as ExpoDevice from "expo-device"
+
+import base64 from "react-native-base64"
+
+const DATA_UUID = "00000001-710e-4a5b-8d75-3e5b444bc3cf"
+const DATA_CHARACTERISTIC = "00000002-710e-4a5b-8d75-3e5b444bc3cf"
+
+const deviceName = "therealdeal"
+
+interface BluetoothLowEnergyApi {
+    requestPermissions(): Promise<boolean>;
+    scanForPeripherals(): void;
+    allDevices: Device[];
+    connectToDevice: (deviceId: Device) => Promise<void>;
+    connectedDevice: Device | null;
+    data: number;
+    disconnectFromDevice() : void;
+}
+
+export default function useBLE(): BluetoothLowEnergyApi {
+    const bleManager = useMemo(() => new BleManager(), []);
+    const [allDevices, setAllDevices] = useState<Device[]>([]);
+    const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+    const [data, setData] = useState<number>(-1);
+
+    const requestAndroid31Permissions = async () => {
+        const bluetoothScanPermission = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+            {
+                title: "Scan Permission",
+                message: "App requires Bluetooth Scanning",
+                buttonPositive: "OK",
+            }
+        );
+        const bluetoothConnectPermission = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+            {
+                title: "Connect Permission",
+                message: "App requires Bluetooth Connecting",
+                buttonPositive: "OK",
+            }
+        );
+        const bluetoothFineLocationPermissions = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+                title: "Fine Location",
+                message: "App requires fine location",
+                buttonPositive: "OK",
+            }
+        );
+
+        return (
+            bluetoothScanPermission === "granted" &&
+            bluetoothConnectPermission === "granted" &&
+            bluetoothFineLocationPermissions === "granted"
+        );
+    };
+
+    const requestPermissions = async () => {
+        if(Platform.OS === "android") {
+            if((ExpoDevice.platformApiLevel ?? -1) < 31) {
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                    {
+                        title: "Location Permission",
+                        message: "Bluetooth requires Location",
+                        buttonPositive: "OK"
+                    }
+                );
+
+                return granted === PermissionsAndroid.RESULTS.GRANTED;
+            } else {
+                const isAndroidPermissionGranted = await requestAndroid31Permissions();
+                return isAndroidPermissionGranted
+            }
+        } else {
+            return true;
+        }
+    };
+
+    const isDuplicateDevice = (devices: Device[], nextDevice: Device) =>
+        devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+
+  const scanForPeripherals = () =>
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error);
+      }
+      if (device && device.name?.includes(deviceName)) {
+        setAllDevices((prevState: Device[]) => {
+          if (!isDuplicateDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+      }
+    });
+
+    const connectToDevice = async (device: Device) => {
+        try {
+            const deviceConnection = await bleManager.connectToDevice(device.id)
+            setConnectedDevice(deviceConnection);
+            await deviceConnection.discoverAllServicesAndCharacteristics();
+            bleManager.stopDeviceScan();
+            startStreamingData(deviceConnection);
+        } catch (e) {
+            console.log("ERROR IN CONNECTION", e);
+        }
+        
+    }
+
+    const onDataUpdate = (
+        error: BleError | null,
+        characeristic: Characteristic | null
+    ) => {
+        if(error) {
+            console.log(error);
+            return
+        } else if (!characeristic?.value) {
+            console.log("No Data Received")
+            return
+        }
+
+        const rawData = base64.decode(characeristic.value)
+        setData(rawData)
+    }
+
+    async function sleep(ms: number): Promise<void> {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    const startStreamingData = async (device: Device) => {
+        while(true) {
+            if(device) {
+                const readCharacteristic = await device.readCharacteristicForService(DATA_UUID, DATA_CHARACTERISTIC);
+                const rawData = base64.decode(readCharacteristic.value)
+                setData(rawData)
+                await sleep(10);
+            }
+        }
+        // if(device) {
+        //     device.monitorCharacteristicForService(
+        //         DATA_UUID, DATA_CHARACTERISTIC, onDataUpdate);
+        // } else {
+        //     console.log("No Device Connected")
+        // }
+    }
+
+    const disconnectFromDevice = () => {
+        if(connectedDevice) {
+            bleManager.cancelDeviceConnection(connectedDevice.id)
+            setConnectedDevice(null);
+            setData(-1);
+        }
+    }
+
+    return {
+        scanForPeripherals,
+        requestPermissions,
+        allDevices,
+        connectToDevice,
+        connectedDevice,
+        data,
+        disconnectFromDevice
+    }
+}
+
