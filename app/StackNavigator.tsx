72ef28e94f0cbc47f4e9ff0356d350da6e866113
Harjot Singh <harjot.singh0712@gmail.com>
2024-12-29 17:34:37 -0800

    Add bottom navigation bar to main screen

--- app/StackNavigator.tsx
+++ app/StackNavigator.tsx
@@ -1,9 +1,11 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Login from "./screens/Login";
 import Profile from "./screens/Profile";
 import Settings from "./screens/Settings";
 import Main from "./screens/Main";
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function StackNavigator() {
   return (
@@ -13,9 +15,24 @@ export default function StackNavigator() {
       }}
     >
       <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Main" component={TabNavigator} />
       <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="Settings" component={Settings} />
     </Stack.Navigator>
   );
+}
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Annotation"
+      screenOptions={{
+        sceneStyle: { backgroundColor: '#fff' },
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen name="Annotation" component={Main} />
+      <Tab.Screen name="Profile" component={Settings} />
+    </Tab.Navigator>
+  )
 }
