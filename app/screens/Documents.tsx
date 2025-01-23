afc685217e15da7e5a050df535eb1a0a8a5c718a
Harjot Singh <harjot.singh0712@gmail.com>
2025-01-22 20:34:48 -0800

    Make documents scrollable

--- app/screens/Documents.tsx
+++ app/screens/Documents.tsx
@@ -2,7 +2,7 @@ import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import PagePreview from "../components/PagePreview";
 import { useIsFocused } from "@react-navigation/native";
@@ -21,26 +21,27 @@ export default function Documents() {
       .catch(console.error);
   }, [focused])
 
-  const updateAndRedirect = async () => {
-    const files = documents;
-
-    let i: number = 0;
-    while (files.has(`untitled${i}.ispen`)) {
-      i++;
+  useEffect(() => {
+    const saveFile = async () => {
+      try {
+        await AsyncStorage.setItem('files', JSON.stringify(Array.from(documents.entries())));
+      } catch (err) {
+        console.warn(err);
+      }
     }
-    const fileName: string = `untitled${i}.ispen`;
-    const newFile: fileInfo = { pages: [] };
+    saveFile();
+  }, [documents])
 
-    files.set(fileName, newFile);
-
-    try {
-      await AsyncStorage.setItem('files', JSON.stringify(Array.from(files.entries())));
-    } catch (err) {
-      console.warn(err);
-    }
-    setDocuments(files)
-
-    navigation.navigate('Editor', { fileName: fileName });
+  const updateAndRedirect = () => {
+    setDocuments(map => {
+      let i: number = 0;
+      while (map.has(`untitled${i}.ispen`)) {
+        i++;
+      }
+      const fileName = `untitled${i}.ispen`;
+      const newFile: fileInfo = { pages: [] };
+      return new Map(map.set(fileName, newFile))
+    });
   }
 
   const openFile = async (file: string) => {
@@ -59,6 +60,24 @@ export default function Documents() {
     });
   }, [navigation]);
 
+  interface itemProps {
+    title: string;
+    fInfo: fileInfo;
+  }
+
+  const Item = ({ title, fInfo }: itemProps) => (
+    <TouchableOpacity
+      onPress={() => openFile(title)}
+      className="h-40 w-full m-2 p-1 flex bg-gray-100"
+      style={{ flexBasis: '30%' }} // Ensures 3 items per row
+    >
+      <View className="flex-1 w-full border bg-white">
+        <PagePreview paths={fInfo.pages[0]} />
+      </View>
+      <Text className="text-xs flex-initial font-bold">{title}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View
       style={{
@@ -67,15 +86,15 @@ export default function Documents() {
         paddingLeft: insets.left,
         paddingRight: insets.right
       }}
-      className="h-full w-full flex flex-row flex-wrap justify-center">
-      {Array.from(documents).map(([name, data], index) => (
-        <TouchableOpacity onPress={() => openFile(name)} key={index} className="h-1/4 w-2/5 m-2 flex bg-gray-100 p-1">
-          <View className="flex-1 w-full border bg-white">
-            <PagePreview paths={data.pages[0]} />
-          </View>
-          <Text className="text-xs flex-initial font-bold">{name}</Text>
-        </TouchableOpacity>
-      ))}
+      className="h-full w-full">
+      <FlatList
+        data={Array.from(documents)}
+        renderItem={({ item }) => <Item title={item[0]} fInfo={item[1]} />}
+        keyExtractor={item => item[0]}
+        className="w-full h-full"
+        contentContainerStyle={{ alignItems: "center" }}
+        numColumns={3}
+      />
     </View>
   )
 }
