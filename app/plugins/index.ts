ade40c156d1f160eff1ab25d23d57311a66a3e96
TheHS1 <harjot.singh0712@gmail.com>
2025-02-22 18:15:17 -0800

    Add weather plugin for testing purpose

--- app/plugins/index.ts
+++ app/plugins/index.ts
@@ -1,7 +1,8 @@
 import { PlugInfo } from '../PluginManager';
 import LatexPlugin from './LatexPlugin';
 import SentimentPlugin from './SentimentPlugin';
+import WeatherPlugin from './WeatherPlugin';
 
 export default function plugArray(): PlugInfo[] {
-    return [LatexPlugin(), SentimentPlugin()];
+    return [WeatherPlugin(), LatexPlugin(), SentimentPlugin()];
 }
