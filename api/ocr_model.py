ecea6386c05d946395082e89d6aa0088ecbb798d
TheHS1 <harjot.singh0712@gmail.com>
2025-04-29 16:11:58 -0700

    Use GPU for accelerating AI inference

--- api/ocr_model.py
+++ api/ocr_model.py
@@ -7,12 +7,13 @@ import logging
 # hold ocr model so it doesn't have to be loaded over and over
 model = None
 processor = None
-device = "cpu"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 # load the ocr model as soon as the module is loaded
 try:
-    model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True).to(device)
-    processor = AutoProcessor.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-large", trust_remote_code=True).to(device, torch_dtype)
+    processor = AutoProcessor.from_pretrained("microsoft/Florence-2-large", trust_remote_code=True)
     logging.info("Loaded OCR model")
 except Exception as e:
     logging.error(f"Error loading OCR model: {e}")
@@ -37,29 +38,28 @@ def ocr(image_data_bytes):
 
     # Define the prompt for OCR
     prompt = '<OCR_WITH_REGION>'
-    inputs = processor(text=prompt, images=image, return_tensors="pt")
+    inputs = processor(text=prompt, images=image, return_tensors="pt").to(device, torch_dtype)
 
     # Move tensors to the correct device and dtype
-    inputs = {
-        k: v.to(device=device, dtype=torch.int64) if k == "input_ids" else v.to(device=device, dtype=torch.float32)
-        if torch.is_tensor(v) else v
-        for k, v in inputs.items()
-    }
+    # inputs = {
+    #     k: v.to(device=device, dtype=torch.int64) if k == "input_ids" else v.to(device=device, dtype=torch.float32)
+    #     if torch.is_tensor(v) else v
+    #     for k, v in inputs.items()
+    # }
 
     # Generate the output using the model
-    generated_ids = model.generate(
-        input_ids=inputs["input_ids"],
-        pixel_values=inputs["pixel_values"],
-        max_new_tokens=4096,
-        num_beams=3
-    )
+    with torch.no_grad():
+        generated_ids = model.generate(
+            input_ids=inputs["input_ids"],
+            pixel_values=inputs["pixel_values"],
+            max_new_tokens=4096,
+            num_beams=3,
+            do_sample=False
+        )
 
     # Decode and return the generated text and bounds
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
-    print(generated_text)
     parsed_answer = processor.post_process_generation(generated_text, task=prompt, image_size=(image.width, image.height))
     print(parsed_answer)
     return parsed_answer['<OCR_WITH_REGION>']['labels']
-
-
 
